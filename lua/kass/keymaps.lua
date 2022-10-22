369a923d879393627e3eab12d5cb6dd44d6a60bc
kasshedi117 <medhedhi.kestouri@esprit.tn>
2022-10-22 21:58:10 +0200

    add basic keymaps [feature/keymaps]

--- lua/kass/keymaps.lua
+++ lua/kass/keymaps.lua
@@ -1,1 +1,31 @@
-vim.keymap.set("i", "jj", "<Esc>", options)
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Sorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Rename Space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("i", "jj", "<Esc>", opts)
+keymap("i", "bb", "/*", opts)
+keymap("i", "nn", "*/", opts)
+keymap("i", "BB", "<!--", opts)
+keymap("i", "NN", "-->", opts)
+
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+keymap("v", "p", '"_dP', opts)
