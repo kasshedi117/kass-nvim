3581c7d61ec2f62719dec5c4053632768d1c6d85
kasshedi117 <medhedhi.kestouri@esprit.tn>
2022-10-23 23:45:50 +0200

    add shortcut <leader>h to visualMode

--- lua/kass/whichkey.lua
+++ lua/kass/whichkey.lua
@@ -92,7 +92,7 @@ local mappings = {
 
 local vopts = {
   mode = "v", -- VISUAL mode
-  prefix = "<leader>",
+  prefix = "<leader>h",
   buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
   silent = true, -- use `silent` when creating keymaps
   noremap = true, -- use `noremap` when creating keymaps
