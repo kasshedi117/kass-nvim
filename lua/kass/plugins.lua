ab18978d77eb59309ab3d7ea9c364297718c2eb8
Hedi Kastouri <hedi.kastouri@c24.de>
2022-10-25 21:25:13 +0200

    add gitsigns to packer [feature/gitsigns]

--- lua/kass/plugins.lua
+++ lua/kass/plugins.lua
@@ -102,6 +102,9 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim" -- Easily comment stuff
   use 'JoosepAlviste/nvim-ts-context-commentstring'
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
