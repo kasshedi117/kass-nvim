324e4d8b5aade840014823184f988456ff2629a5
Hedi Kastouri <hedi.kastouri@c24.de>
2022-10-25 23:10:04 +0200

    add Lualine to Packer [feature/lualine]

--- lua/kass/plugins.lua
+++ lua/kass/plugins.lua
@@ -105,6 +105,9 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
+  -- lualine 
+  use { "nvim-lualine/lualine.nvim" }
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
